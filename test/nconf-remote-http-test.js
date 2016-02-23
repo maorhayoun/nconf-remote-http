9ae81b5731fb9b48b7761122292c12158bf00de2
Maor Hayun <maor.hayun@gmail.com>
2016-02-23 16:28:31 +0200

    travis build issues

--- test/nconf-remote-http-test.js
+++ test/nconf-remote-http-test.js
@@ -4,18 +4,16 @@ var nock = require('nock');
 require('../lib/nconf-remote-http');
 
 describe('nconf use()', function () {
-  it('should not throw error when using etcd', function (done) {
+  it('should load json configuration', function (done) {
     nock('http://example')
       .get('/config.json')
       .reply(200, { 'connection-string': 'Data Source=mydb'});
       
-    nconf.use('http', 
+     nconf.use('http', 
       { 
         url: 'http://example/config.json',
         callback: (data) => {
-          console.log(data);
-          console.log(nconf.get('connection-string'));
-          nconf.get('connection-string').should.equal('Data Source=mydb')
+          nconf.get('connection-string').should.equal('Data Source=mydb');
           done();
         }
       });
